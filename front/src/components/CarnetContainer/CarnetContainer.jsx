ddb292ba48eb111b8ac5d199077fa974fdf0c7a6
Fernando <ferestga8@gmail.com>
2023-11-04 14:20:41 +0100

    Added Carnets Section

---
+++ front/src/components/CarnetContainer/CarnetContainer.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react"
+
+const fetchCarnets = async () => {
+    const response = await fetch(`http://localhost:3000/api/carnet`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.getItem("jwt")}`
+        }
+    })
+    if(response.ok) {
+        const data = await response.json()
+        return data
+    }
+    return null
+
+}
+
+export default function CarnetContainer() {
+    const [carnets, setCarnets] = useState([])
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        fetchCarnets().then(data => {
+            setCarnets(data)
+            setLoading(false)
+        })
+    }, [])
+    return (
+        <>
+        <div id="CarnetContainer">
+        <h1 className="carnetsTitle">CARNETS</h1>
+        <div id="MultiCarnetContainer">
+            {carnets && !loading &&  
+                carnets.map(carnet => (
+                    <div className="CarnetBox" key={carnet._id}>
+                        <section>
+                            <img src={carnet.streamer.profile_url} alt="streamer img" />
+                        </section>
+                        <section>
+                            <h2>{carnet.streamer.name}</h2>
+                        </section>
+                    </div>
+                ))
+        }
+        </div>
+        </div>
+        </>
+    )
+}
