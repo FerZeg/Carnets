630ce75de400c7e6c3961b2ed8903c503f3f85d9
Fernando <ferestga8@gmail.com>
2023-11-06 13:29:46 +0100

    Small fixes

--- front/src/pages/Streamers/StreamersPage.jsx
+++ front/src/pages/Streamers/StreamersPage.jsx
@@ -1,5 +1,6 @@
 import './StreamersPage.css'
 import { fetchStreamers } from '../../lib/fetchers'
+import { Link } from 'react-router-dom'
 
 import { useState, useEffect } from 'react';
 export default function StreamersPage() {
@@ -19,10 +20,12 @@ export default function StreamersPage() {
             <div id="StreamerContainer">
             {!loading && streamers.length > 0 && (
                 streamers.map(streamer => (
-                    <div key={streamer._id}>
-                        <h2>{streamer.display_name}</h2>
-                        <img src={streamer.profile_image_url} alt="streamer img" />
-                    </div>
+                    <Link to={"/" + streamer.display_name} key={streamer._id}>
+                        <div className="StreamerBox">
+                            <h2>{streamer.display_name}</h2>
+                            <img src={streamer.profile_image_url} alt="streamer img" />
+                        </div>
+                    </Link>
                 ))
             )}
             </div>
