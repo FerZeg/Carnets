bd57f589fcf42260f37666552d4947fd8a5ed6d9
Fernando <ferestga8@gmail.com>
2023-11-07 10:37:07 +0100

    Remove logs

--- src/Routes/apiV1/streamer.js
+++ src/Routes/apiV1/streamer.js
@@ -4,7 +4,6 @@ import User from "../../Models/UserModel.js"
 import Streamer from "../../Models/StreamerModel.js"
 import fs from "fs"
 const streamerValidation = JSON.parse(fs.readFileSync("streamers.json", "utf8"))
-console.log(streamerValidation)
 const router = Router()
 router.get("/", async (req, res, next) => {
 	const {max, offset} = req.query
@@ -18,7 +17,6 @@ router.get("/", async (req, res, next) => {
 router.post("/upgrade", verifyAuthTokenMiddleware, async (req, res, next) => {
 	try {
 		const user = await User.getUserById(req.user.id)
-		console.log(user.display_name)
 		if(!streamerValidation.names.includes(user.display_name)) throw new Error("No puedes ser streamer")
 		const id = req.user.id
 		await User.changeUserType(id, "streamer")
