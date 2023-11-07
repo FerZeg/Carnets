fcaac26f1f62982170696ac7b3fba012ea15b141
Fernando <ferestga8@gmail.com>
2023-11-07 18:01:27 +0100

    Ranking functionality added

--- src/Models/StreamerModel.js
+++ src/Models/StreamerModel.js
@@ -8,6 +8,9 @@ const Streamer = {
 		if(result.insertedCount === 0) throw new BadRequestError("No se pudo crear el streamer")
 		return result
 	},
+	getStreamerByName: async (name) => {
+		return await streamerCollection.findOne({display_name: name})
+	},
 	getStreamers: async (max = 25, n = 0) => {
 		if(max > 25) throw new BadRequestError("No se pueden mostrar más de 25 streamers")
 		return await streamerCollection.find({type: "streamer"}).project({id: 1, display_name: 1, twitch_type: 1, profile_image_url: 1  }).skip(n * max).limit(max).toArray()
