a5d71144008f7822b735508018f2e4cc2530abb8
Fernando <ferestga8@gmail.com>
2023-12-18 19:12:38 +0100

    Fix a Bug with twitch tokens

--- src/conexion.js
+++ src/conexion.js
@@ -1,5 +1,5 @@
 import {MongoClient} from "mongodb"
-const client = new MongoClient(process.env.DB_URL)
+const client = new MongoClient("mongodb://localhost:27017")
 const db = client.db("Carnets")
 async function connect() {
 	try {
