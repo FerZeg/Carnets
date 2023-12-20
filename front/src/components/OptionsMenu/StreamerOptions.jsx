125992989de1e25e0169cfc9130bbe974a2d4a37
Fernando <ferestga8@gmail.com>
2023-12-20 23:58:27 +0100

    Add vercel deploy

--- front/src/components/OptionsMenu/StreamerOptions.jsx
+++ front/src/components/OptionsMenu/StreamerOptions.jsx
@@ -1,27 +1,28 @@
 const StreamerOptions = () => {
     return (
     <section className="OptionsContainer">
+        <h2 style={{fontSize:"1.5rem"}}>STREAMER</h2>
                 <div className="Option">
-                    <h2>Usar recompensa limitada diaria</h2>
+                    <h3>Usar recompensa limitada diaria</h3>
                     <div className="SwitchContainer">
                         <input type="checkbox" className="checkbox" name='daily' id='dailycheck'/>
-                        <label className="switch" htmlFor="dailycheck">
+                        <label className="switch" htmlFor="daily">
                             <span className="slider"></span>
                         </label>
                     </div>
-                    <label htmlFor="Puntos">Puntos necesarios:</label>
-                    <input type="text" name="Puntos"/>
+                    <label htmlFor="daily_points">Puntos necesarios:</label>
+                    <input type="text" name="daily_points"/>
                 </div>
                 <div className="Option">
-                    <h2>Usar recompensa por stream</h2>
+                    <h3>Usar recompensa por stream</h3>
                     <div className="SwitchContainer">
-                        <input type="checkbox" className="checkbox" name='perstream' id='perstream'/>
-                        <label className="switch" htmlFor="perstream">
+                        <input type="checkbox" className="checkbox" name='per_stream' id='perstream'/>
+                        <label className="switch" htmlFor="pers_tream">
                             <span className="slider"></span>
                         </label>
                     </div>
-                    <label htmlFor="Puntos">Puntos necesarios:</label>
-                    <input type="text" name="Puntos"/>
+                    <label htmlFor="per_stream_points">Puntos necesarios:</label>
+                    <input type="text" name="per_stream_points"/>
                 </div>
             </section>
     )
